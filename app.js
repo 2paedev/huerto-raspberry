7020f3f9534eaae5ee89c0e2299c16281cad6264
Diego M. Martínez Ayuso <diego.martinez.ayuso@gmail.com>
2018-10-09 15:12:48 +0200

    feat(cemera):camera test in progress

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 const port = 80;
 const routes = require('./routes');
 const ajaxRoute = require('./routes/ajax');
+const sockets = {};
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -25,6 +29,63 @@ app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
 
 app.use('/', routes);
 app.post('/ajax', ajaxRoute);
+
+io.on('connection', function(socket) {
+  sockets[socket.id] = socket;
+  console.log('Total clients connected : ', Object.keys(sockets).length);
+
+  socket.on('disconnect', function() {
+    delete sockets[socket.id];
+
+    // no more sockets, kill the stream
+    if (Object.keys(sockets).length == 0) {
+      app.set('watchingFile', false);
+      if (proc) proc.kill();
+      fs.unwatchFile('./stream/image_stream.jpg');
+    }
+  });
+
+  socket.on('start-stream', function() {
+    startStreaming(io);
+  });
+});
+
+function stopStreaming() {
+  if (Object.keys(sockets).length == 0) {
+    app.set('watchingFile', false);
+    if (proc) proc.kill();
+    fs.unwatchFile('./stream/image_stream.jpg');
+  }
+}
+
+function startStreaming(io) {
+  if (app.get('watchingFile')) {
+    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + Math.random() * 100000);
+    return;
+  }
+
+  var args = [
+    '-w',
+    '640',
+    '-h',
+    '480',
+    '-o',
+    './stream/image_stream.jpg',
+    '-t',
+    '999999999',
+    '-tl',
+    '100'
+  ];
+  proc = spawn('raspistill', args);
+
+  console.log('Watching for changes...');
+
+  app.set('watchingFile', true);
+
+  fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
+    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + Math.random() * 100000);
+  });
+}
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- public/js/camera.js
+++ public/js/camera.js
@@ -2,4 +2,16 @@ $(function() {
   $('.button-go-back').click(function() {
     window.location.href = '/';
   });
+
+  var socket = io();
+
+  $('button-camera').click(function() {
+    socket.emit('start-stream');
+    $('.button-camera').hide();
+  });
+
+  socket.on('liveStream', function(url) {
+    $('#stream').attr('src', url);
+    $('.button-camera').hide();
+  });
 });
